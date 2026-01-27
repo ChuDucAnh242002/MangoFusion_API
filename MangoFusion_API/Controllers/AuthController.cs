f496ca9c38f17c8f38bef8be2930ac7bedcb755e
ChuDucAnh242002 <anh.chu@tuni.fi>
2026-01-27 20:05:35 +0200

    Add Login endpoint

--- MangoFusion_API/Controllers/AuthController.cs
+++ MangoFusion_API/Controllers/AuthController.cs
@@ -3,7 +3,11 @@ using MangoFusion_API.Models.Dto;
 using MangoFusion_API.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 
 namespace MangoFusion_API.Controllers
 {
@@ -18,7 +22,7 @@ namespace MangoFusion_API.Controllers
 
         public AuthController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
-            secretKey = configuration.GetValue<string>("Application:Secret") ?? "";
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret") ?? "";
             _response = new ApiResponse();
             _userManager = userManager;
             _roleManager = roleManager;
@@ -72,6 +76,75 @@ namespace MangoFusion_API.Controllers
                     _response.IsSuccess = false;
                     return BadRequest(_response);
                 }
+            }
+            else
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                foreach (var error in ModelState.Values)
+                {
+                    foreach (var item in error.Errors)
+                    {
+                        _response.ErrorMessages.Add(item.ErrorMessage);
+                    }
+                }
+                return BadRequest(_response);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userFromDb = await _userManager.FindByEmailAsync(model.Email);
+                if (userFromDb != null)
+                {
+                    bool isValid = await _userManager.CheckPasswordAsync(userFromDb, model.Password);
+                    if (!isValid)
+                    {
+                        _response.Result = new LoginResponseDto();
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        _response.ErrorMessages.Add("Invalid Credentials");
+                        return BadRequest(_response);
+                    }
+                    JwtSecurityTokenHandler tokenHandler = new();
+                    byte[] key = Encoding.ASCII.GetBytes(secretKey);
+
+                    SecurityTokenDescriptor tokenDescriptor = new()
+                    {
+                        Subject = new ClaimsIdentity(
+                            [
+                            new ("fullname", userFromDb.Name),
+                            new ("id", userFromDb.Id),
+                            new (ClaimTypes.Email, userFromDb.Email!.ToString()),
+                            new (ClaimTypes.Email, _userManager.GetRolesAsync(userFromDb).Result.FirstOrDefault()!)
+                            ]),
+                        Expires = DateTime.UtcNow.AddDays(7),
+                        SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+                    LoginResponseDto loginResponse = new()
+                    {
+                        Email = userFromDb.Email,
+                        Token = tokenHandler.WriteToken(token),
+                        Role = _userManager.GetRolesAsync(userFromDb).Result.FirstOrDefault()!,
+                    };
+
+                    _response.Result = loginResponse;
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
+
+                _response.Result = new LoginResponseDto();
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Invalid Credentials");
+                return BadRequest(_response);
             }
             else
             {
