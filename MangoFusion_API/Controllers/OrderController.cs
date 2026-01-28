31f57c75080bb9f608cb1138dff66d7ff5dad6bd
ChuDucAnh242002 <anh.chu@tuni.fi>
2026-01-28 16:37:13 +0200

    Initialize createOrder endpoint

---
+++ MangoFusion_API/Controllers/OrderController.cs
@@ -0,0 +1,123 @@
+﻿using MangoFusion_API.Data;
+using MangoFusion_API.Models;
+using MangoFusion_API.Models.Dto;
+using MangoFusion_API.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace MangoFusion_API.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ApiResponse _response;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public ActionResult<ApiResponse> GetOrders(string userId = "")
+        {
+            IEnumerable<OrderHeader> orderHeaderList = _db.OrderHeaders.Include(u => u.OrderDetails)
+                .ThenInclude(u => u.MenuItem).OrderByDescending(u => u.OrderHeaderId);
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                orderHeaderList = orderHeaderList.Where(u => u.ApplicationUserId == userId);
+            }
+
+            _response.Result = orderHeaderList;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+
+        [HttpGet("{orderId:int}")]
+        public ActionResult<ApiResponse> GetOrder(int orderId)
+        {
+            if (orderId == 0)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.ErrorMessages.Add("Invalid order Id");
+                return BadRequest(_response);
+            }
+
+            OrderHeader? orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails)
+                .ThenInclude(u => u.MenuItem).FirstOrDefault(u => u.OrderHeaderId == orderId);
+
+            if (orderHeader == null)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.ErrorMessages.Add("Order not found");
+                return NotFound(_response);
+            }
+
+            _response.Result = orderHeader;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        public ActionResult<ApiResponse> CreateOrder([FromBody] OrderHeaderCreateDTO orderHeaderDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    OrderHeader orderHeader = new()
+                    {
+                        PickUpName = orderHeaderDTO.PickUpName,
+                        PickUpPhoneNumber = orderHeaderDTO.PickUpPhoneNumber,
+                        PickUpEmail = orderHeaderDTO.PickUpEmail,
+                        OrderDate = DateTime.Now,
+                        Status = SD.status_confirmed,
+                        TotalItem = orderHeaderDTO.TotalItem,
+                        ApplicationUserId = orderHeaderDTO.ApplicationUserId,
+                    };
+
+                    _db.OrderHeaders.Add(orderHeader);
+                    _db.SaveChanges();
+
+                    foreach(var orderDetailDTO in orderHeaderDTO.OrderDetails)
+                    {
+                        OrderDetail orderDetail = new()
+                        {
+                            OrderHeaderId = orderHeader.OrderHeaderId,
+                            MenuItemId = orderDetailDTO.MenuItemId,
+                            Quantity = orderDetailDTO.Quantity,
+                            ItemName = orderDetailDTO.ItemName,
+                            Price = orderDetailDTO.Price,
+                        };
+                        _db.OrderDetails.Add(orderDetail);
+                    }
+                    _db.SaveChanges();
+                    _response.Result = orderHeader;
+                    orderHeader.OrderDetails = [];
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtAction(nameof(GetOrder), new { orderId = orderHeader.OrderHeaderId }, _response);
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = ModelState.Values.SelectMany(u => u.Errors).Select(u => u.ErrorMessage).ToList();
+                    return BadRequest(_response);
+                }
+
+                
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
--- MangoFusion_API/Utility/SD.cs
+++ MangoFusion_API/Utility/SD.cs
@@ -4,5 +4,10 @@
     {
         public const string Role_Admin = "Admin";
         public const string Role_Customer = "Customer";
+
+        public const string status_confirmed = "Confirmed";
+        public const string status_readyForPickUp = "Ready for Pickup";
+        public const string status_completed = "Completed";
+        public const string status_cancelled = "Cancelled";
     }
 }
